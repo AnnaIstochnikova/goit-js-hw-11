04ed00b46c8bcf5ce37bf0a8818655a0c70982cb
AnnaIstochnikova <anya.istochnikova@gmail.com>
2023-09-07 20:13:03 +0200

    add js, change css and html

--- src/index.js
+++ src/index.js
@@ -1,37 +1,58 @@
 'use strict';
 import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-//const options = {};
+Notiflix.Notify.init({
+  width: '400px',
+  position: 'center-center',
+  failure: {
+    background: '#F370E2',
+    textColor: '#fff',
+  },
+});
+
 let requestedWord = '';
-const gallery = document.querySelector('gallery');
+const gallery = document.querySelector('.gallery');
 const btn = document.querySelector('.button');
 const input = document.querySelector('.searchQuery');
-
-const API_KEY = '39259694-dab3c4ff3451cd0969a895f6a';
-axios.defaults.headers.common['x-api-key'] = API_KEY;
 
 input.addEventListener('change', function eventHandler(event) {
   event.preventDefault();
   requestedWord = event.target.value.toLowerCase().replace(/\s+/g, '+');
-  console.log(requestedWord);
+  //console.log(requestedWord);
   const fetchData = async () => {
     const response = await axios.get(
       `https://pixabay.com/api/?key=39259694-dab3c4ff3451cd0969a895f6a&q=${requestedWord}`,
       {
         params: {
-          //q: requestedWord,
-          //image_type: 'photo',
+          safesearch: true,
+          orientation: 'horizontal',
+          image_type: 'photo',
+          per_page: 40,
         },
       }
     );
-    const data = await response.json();
-    return data;
+    console.log(response.data);
+    return response.data;
   };
 
   btn.addEventListener('click', function runButton(event) {
     event.preventDefault();
     fetchData()
-      .then(data => console.log(data))
+      .then(data => {
+        gallery.innerHTML = '';
+        renderPhotos(data.hits);
+        if (data.hits.length === 0) {
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        }
+        if (data.hits.length !== 0) {
+          Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        }
+      })
       .catch(error => console.log(error.message));
   });
 });
@@ -39,22 +60,32 @@ input.addEventListener('change', function eventHandler(event) {
 function renderPhotos(photos) {
   photos.forEach(photo => {
     const addPhotoToSelect = `<div class="photo-card">
+    <a class="gallery__link" href="${photo.largeImageURL}">
       <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
+      </a>
       <div class="info">
         <p class="info-item">
-          <b>Likes: ${photo.likes}</b>
+          <b>Likes</b> 
+          <b>${photo.likes}</b>
         </p>
         <p class="info-item">
-          <b>Views: ${photo.views}</b>
+          <b>Views</b> 
+          <b>${photo.views}</b>
         </p>
         <p class="info-item">
-          <b>Comments: ${photo.comments}</b>
+          <b>Comments</b> 
+          <b>${photo.comments}</b>
         </p>
         <p class="info-item">
-          <b>Downloads: ${photo.downloads}</b>
+          <b>Downloads</b>
+          <b>${photo.downloads}</b>
         </p>
       </div>
     </div>`;
+    let simpleLightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
     gallery.insertAdjacentHTML('afterbegin', addPhotoToSelect);
   });
 }
